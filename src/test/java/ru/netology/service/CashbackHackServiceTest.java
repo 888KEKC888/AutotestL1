a76483ba7f233ddb07d1a3fced2d1295205ae9f4
888KEKC888 <sitneckov@mail.ru>
2024-07-28 16:15:28 +0300

    add new test in testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -17,4 +17,15 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
 
     }
+
+    @Test
+    public void shouldResultZero() {
+        CashbackHackService hackService = new CashbackHackService();
+        int amaunt = 1000;
+
+        int actual = hackService.remain(amaunt);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
 }
